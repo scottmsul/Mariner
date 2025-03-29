d9be8cecd16a7fb4740dd80343bc39c31afa53d8
abigal3n <abbykittygaming13@gmail.com>
2025-03-29 17:42:06 -0400

    go to max velocity and acceleration increased

--- src/main/java/frc/robot/commands/GoTo.java
+++ src/main/java/frc/robot/commands/GoTo.java
@@ -51,7 +51,7 @@ public class GoTo {
                 return DriverStation.getAlliance().orElse(Alliance.Blue);
         }
 
-        public static PathConstraints constraints = new PathConstraints(2, 2.35, 360, 360);
+        public static PathConstraints constraints = new PathConstraints(3.5, 3, 360 * 1.5, 360);
 
         private static Pose2d inFrontOfTag(int id) {
                 Transform2d rot180 = new Transform2d(Translation2d.kZero, Rotation2d.k180deg);
