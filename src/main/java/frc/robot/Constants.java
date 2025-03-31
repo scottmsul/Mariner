099c450d5f8b227bc6cc5e4bf54f7abf95f3960a
abigal3n <abbykittygaming13@gmail.com>
2025-03-31 15:55:09 -0400

    more cs tuning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -225,7 +225,7 @@ public final class Constants {
       public static NTDouble processor = new NTDouble(-0.05, "processor", "StrafeOffsets");
       public static NTDouble l1Left = new NTDouble(0.10, "l1Left", "StrafeOffsets");
       public static NTDouble l1Right = new NTDouble(0.10, "l1Right", "StrafeOffsets");
-      public static NTDouble csStrafe = new NTDouble(0.17, "Coral Station Strafe", "StrafeOffsets");
+      public static NTDouble csStrafe = new NTDouble(0.18, "Coral Station Strafe", "StrafeOffsets");
       // static double CoralSt = 0;
     }
 
